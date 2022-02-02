66a864deb000e7c925650e51c3196cdf1c32e426
Cyprian Abeiku Maison <84200294+camaison@users.noreply.github.com>
2022-02-02 19:14:27 +0000

    Hangman Game Project
    The hamgman.py file imports all the words in the wordlist.py file and a random word from this list of words is assigned to a variable as long as the word meets a certain criteria described in the function.
    
    A random word is assigned each time the loop runs and the player is asked to guess the word by entering letters and those letters are compared to the set of letters that make up the right answer. If the letters entered form the right word before there are no more lives, the player wins!

---
+++ Hangman Game Project/hangman.py
@@ -0,0 +1,46 @@
+from wordlist import word_list as words
+import random
+import string 
+
+def word_corrector(words):
+    word = random.choice(words)
+    while "-" in word or " " in word or len(word)<4: 
+        word = random.choice(words)
+    return word
+    
+
+def hangman():
+    word = word_corrector(words)
+    print(word)
+    word_letters = set(word)
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set()
+    lives = len(word) + 2
+    attempts = 0
+    while len(word_letters)> 0 and lives > 0:
+        if attempts > 0:
+            print("You have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Word: ", " ".join(word_list))
+        guess = input("Guess a Letter: ").lower()
+        attempts+=1
+
+        if guess not in used_letters and guess in alphabet:
+            used_letters.add(guess)
+
+            if guess in word_letters:
+                word_letters.remove(guess)
+            else:
+                lives -= 1
+                print(f"Wrong guess! Lives left:{lives}")
+                if lives == 0:
+                    print(f"Game over!\nThe word is {word} ") 
+
+        elif guess in used_letters:
+            print("You have already used this letter!")
+
+        else:
+            print("Invalid Character!")
+            
+hangman()
